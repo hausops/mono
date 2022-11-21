f14105ad5faeaf6011fe4c818e25908e194f3025
Thanik Bhongbhibhat <email@thanik.me>
2022-11-21 12:08:29 +0800

    Create layouts: Page, PageHeader

---
+++ apps/dashboard/layouts/PageHeader.tsx
@@ -0,0 +1,16 @@
+import {ReactNode} from 'react';
+import * as s from './PageHeader.css';
+
+type PageHeaderProps = {
+  title: string;
+  actions?: ReactNode;
+};
+
+export default function PageHeader({title, actions}: PageHeaderProps) {
+  return (
+    <header className={s.Header}>
+      <h1 className={s.Title}>{title}</h1>
+      {actions && <div className={s.Actions}>{actions}</div>}
+    </header>
+  );
+}
--- apps/dashboard/pages/properties/index.css.ts
+++ apps/dashboard/pages/properties/index.css.ts
@@ -1,28 +1,5 @@
 import {unit} from '@/volto/spacing.css';
-import {font} from '@/volto/typography.css';
 import {style} from '@vanilla-extract/css';
-
-// TODO: extract to Page layout
-export const Page = style({
-  display: 'flex',
-  flexDirection: 'column',
-  padding: `${unit(8)} ${unit(16)}`,
-  // TBD
-  rowGap: unit(4),
-});
-
-// TODO: extract to AppHeader layout
-// while at it, we should wrap actions using wrapper with flex-shrink: 0
-export const Header = style({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-});
-
-export const Title = style({
-  fontSize: font.size[24],
-  fontWeight: font.weight.semibold,
-});
 
 export const PropertyList = style({
   display: 'grid',
