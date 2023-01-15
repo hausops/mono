54efee0da699f09d0ce5cb13b0f1a3546a7882ee
Thanik Bhongbhibhat <email@thanik.me>
2023-01-15 11:00:10 +0700

    wip: Contact Tooltip portal to body

--- apps/dashboard/components/PropertyDetails/TenantProfile.css.ts
+++ apps/dashboard/components/PropertyDetails/TenantProfile.css.ts
@@ -1,4 +1,6 @@
 import {color} from '@/volto/color.css';
+import {layer} from '@/volto/layer.css';
+import {vars} from '@/volto/root.css';
 import {unit} from '@/volto/spacing.css';
 import {font} from '@/volto/typography.css';
 import {style} from '@vanilla-extract/css';
@@ -26,10 +28,48 @@ export const Name = style({
 });
 
 export const ContactInfo = style({
-  color: color.text.muted,
+  alignItems: 'center',
+  columnGap: unit(1),
+  display: 'flex',
 });
 
-export const Phone = style({
+export const TooltipContainer = style({
+  position: 'absolute',
+  paddingBlock: unit(2),
+  zIndex: layer.tooltip,
+});
+
+export const ContactTooltipContent = style({
+  backgroundColor: color.neutral[35],
+  color: color.neutral[98],
+  fontSize: font.size[14],
+  paddingBlock: unit(2),
+  paddingInline: unit(4),
+  // width: 'max-content',
+});
+
+export const Contact = style({
+  backgroundColor: 'transparent',
+  border: 0,
+  borderRadius: vars.border.radius,
+  color: color.text.muted,
+  cursor: 'pointer',
+  display: 'inline-flex',
+
+  ':hover': {
+    backgroundColor: color.neutral[98],
+    color: color.text.$,
+  },
+
+  ':focus-visible': {
+    backgroundColor: color.neutral[98],
+    color: color.text.$,
+  },
+});
+
+export const ContactSeparator = style({
+  color: color.text.muted,
+  display: 'inline-flex',
   '::before': {
     content: ' • ',
   },
--- apps/dashboard/components/PropertyDetails/TenantProfile.tsx
+++ apps/dashboard/components/PropertyDetails/TenantProfile.tsx
@@ -1,5 +1,9 @@
 import {Avatar} from '@/volto/Avatar';
+import {Card} from '@/volto/Card';
+import {useOverlayPosition, AriaPositionProps} from '@react-aria/overlays';
 import Link from 'next/link';
+import {PropsWithChildren, useRef, useState} from 'react';
+import {createPortal} from 'react-dom';
 import * as s from './TenantProfile.css';
 
 type TenantProfileProps = {
@@ -24,12 +28,101 @@ export function TenantProfile({
         <Link className={s.Name} href="#">
           {name}
         </Link>
-        <p className={s.ContactInfo}>
-          {/* TODO: copy to clipboard on click, needs toast/notify */}
-          <span>{email}</span>
-          {phone && <span className={s.Phone}>{phone}</span>}
-        </p>
+        <div className={s.ContactInfo}>
+          <Contact>{email}</Contact>
+          {phone && (
+            <>
+              <i className={s.ContactSeparator} />
+              <Contact>{phone}</Contact>
+            </>
+          )}
+        </div>
       </div>
     </article>
   );
+}
+
+function Contact({children}: {children: string}) {
+  const tooltipState = useTooltipState();
+  const tooltipTriggerRef = useRef<HTMLButtonElement>(null);
+  return (
+    <>
+      <button
+        className={s.Contact}
+        onMouseEnter={tooltipState.open}
+        onMouseLeave={tooltipState.close}
+        ref={tooltipTriggerRef}
+      >
+        {children}
+      </button>
+      <Tooltip
+        isOpen={tooltipState.isOpen}
+        placement="bottom"
+        targetRef={tooltipTriggerRef}
+      >
+        <Card>
+          <p className={s.ContactTooltipContent}>Copy to clipboard</p>
+        </Card>
+      </Tooltip>
+    </>
+  );
+}
+
+// Tooltip is an element to provide a short context or hint about an UI element.
+
+type TooltipProps = PropsWithChildren<{
+  isOpen: boolean;
+}> &
+  Pick<AriaPositionProps, 'boundaryElement' | 'placement' | 'targetRef'>;
+
+// Tooltip.Overlay
+function Tooltip(props: TooltipProps) {
+  const {boundaryElement, children, isOpen, placement, targetRef} = props;
+
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const {overlayProps} = useOverlayPosition({
+    // boundaryElement,
+    isOpen,
+    overlayRef,
+    placement,
+    targetRef,
+  });
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const position = overlayProps.style ?? {};
+  return createPortal(
+    <div
+      className={s.TooltipContainer}
+      ref={overlayRef}
+      role="tooltip"
+      style={{
+        top: position.top,
+        left: position.left,
+        bottom: position.bottom,
+        right: position.right,
+      }}
+    >
+      {children}
+    </div>,
+    document.body
+  );
+}
+
+function useTooltipState(initialOpen: boolean = false) {
+  const [isOpen, setOpen] = useState(initialOpen);
+  return {
+    isOpen,
+    open() {
+      setOpen(true);
+    },
+    close() {
+      setOpen(false);
+    },
+    toggle() {
+      setOpen(!isOpen);
+    },
+  };
 }
