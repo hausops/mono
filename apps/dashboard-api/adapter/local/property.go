e0226b853ca06d290127e12b2102a996d25cabe0
Thanik Bhongbhibhat <email@thanik.me>
2023-03-15 17:43:02 +0700

    remove type conversion helper in property domain

--- apps/dashboard-api/adapter/local/property.go
+++ apps/dashboard-api/adapter/local/property.go
@@ -13,24 +13,47 @@ func NewPropertyService() *PropertyService {
 	return &PropertyService{byId: make(map[string]property.Property)}
 }
 
-func (r *PropertyService) CreateSingleFamilyProperty(input property.CreateSingleFamilyPropertyInput) (*property.SingleFamilyProperty, error) {
-	p := input.ToProperty()
-	p.ID = uuid.New().String()
-	p.Unit.ID = uuid.New().String()
+func (r *PropertyService) CreateSingleFamilyProperty(in property.CreateSingleFamilyPropertyInput) (*property.SingleFamilyProperty, error) {
+	unit := property.SingleFamilyPropertyUnit{
+		ID:         uuid.New().String(),
+		Bedrooms:   in.Unit.Bedrooms,
+		Bathrooms:  in.Unit.Bathrooms,
+		Size:       in.Unit.Size,
+		RentAmount: in.Unit.RentAmount,
+	}
+
+	p := property.SingleFamilyProperty{
+		ID:            uuid.New().String(),
+		CoverImageURL: in.CoverImageURL,
+		Address:       property.Address(in.Address),
+		BuildYear:     in.BuildYear,
+		Unit:          unit,
+	}
 	r.byId[p.ID] = p
 	return &p, nil
 }
 
-func (r *PropertyService) CreateMultiFamilyProperty(input property.CreateMultiFamilyPropertyInput) (*property.MultiFamilyProperty, error) {
-	units := make([]property.MultiFamilyPropertyUnit, 0, len(input.Units))
-	for _, iu := range input.Units {
-		u := iu.ToUnit()
-		u.ID = uuid.New().String()
+func (r *PropertyService) CreateMultiFamilyProperty(in property.CreateMultiFamilyPropertyInput) (*property.MultiFamilyProperty, error) {
+	units := make([]property.MultiFamilyPropertyUnit, 0, len(in.Units))
+	for _, iu := range in.Units {
+		u := property.MultiFamilyPropertyUnit{
+			ID:         uuid.New().String(),
+			Number:     iu.Number,
+			Bedrooms:   iu.Bedrooms,
+			Bathrooms:  iu.Bathrooms,
+			Size:       iu.Size,
+			RentAmount: iu.RentAmount,
+		}
 		units = append(units, u)
 	}
-	p := input.ToProperty()
-	p.ID = uuid.New().String()
-	p.Units = units
+
+	p := property.MultiFamilyProperty{
+		ID:            uuid.New().String(),
+		CoverImageURL: in.CoverImageURL,
+		Address:       property.Address(in.Address),
+		BuildYear:     in.BuildYear,
+		Units:         units,
+	}
 	r.byId[p.ID] = p
 	return &p, nil
 }
--- apps/dashboard-api/domain/property/service.go
+++ apps/dashboard-api/domain/property/service.go
@@ -19,15 +19,6 @@ type CreateSingleFamilyPropertyInput struct {
 	Unit          CreateSingleFamilyPropertyUnitInput `json:"unit"`
 }
 
-func (in CreateSingleFamilyPropertyInput) ToProperty() SingleFamilyProperty {
-	return SingleFamilyProperty{
-		CoverImageURL: in.CoverImageURL,
-		Address:       in.Address.ToAddress(),
-		BuildYear:     in.BuildYear,
-		Unit:          in.Unit.ToUnit(),
-	}
-}
-
 type CreateSingleFamilyPropertyUnitInput struct {
 	Bedrooms   *float64 `json:"bedrooms"`
 	Bathrooms  *float64 `json:"bathrooms"`
@@ -35,28 +26,11 @@ type CreateSingleFamilyPropertyUnitInput struct {
 	RentAmount *float64 `json:"rentAmount"`
 }
 
-func (in CreateSingleFamilyPropertyUnitInput) ToUnit() SingleFamilyPropertyUnit {
-	return SingleFamilyPropertyUnit{
-		Bedrooms:   in.Bedrooms,
-		Bathrooms:  in.Bathrooms,
-		Size:       in.Size,
-		RentAmount: in.RentAmount,
-	}
-}
-
 type CreateMultiFamilyPropertyInput struct {
 	CoverImageURL *string                              `json:"coverImageUrl"`
 	Address       NewAddressInput                      `json:"address"`
 	BuildYear     *int                                 `json:"buildYear"`
 	Units         []CreateMultiFamilyPropertyUnitInput `json:"units"`
-}
-
-func (in CreateMultiFamilyPropertyInput) ToProperty() MultiFamilyProperty {
-	return MultiFamilyProperty{
-		CoverImageURL: in.CoverImageURL,
-		Address:       in.Address.ToAddress(),
-		BuildYear:     in.BuildYear,
-	}
 }
 
 type CreateMultiFamilyPropertyUnitInput struct {
@@ -67,24 +41,10 @@ type CreateMultiFamilyPropertyUnitInput struct {
 	RentAmount *float64 `json:"rentAmount"`
 }
 
-func (in CreateMultiFamilyPropertyUnitInput) ToUnit() MultiFamilyPropertyUnit {
-	return MultiFamilyPropertyUnit{
-		Number:     in.Number,
-		Bedrooms:   in.Bedrooms,
-		Bathrooms:  in.Bathrooms,
-		Size:       in.Size,
-		RentAmount: in.RentAmount,
-	}
-}
-
 type NewAddressInput struct {
 	Line1 string  `json:"line1"`
 	Line2 *string `json:"line2"`
 	City  string  `json:"city"`
 	State string  `json:"state"`
 	Zip   string  `json:"zip"`
-}
-
-func (in NewAddressInput) ToAddress() Address {
-	return Address(in)
 }
