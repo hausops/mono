4151bcf2ceff14ee3898054d311612f39aeb3e6c
Thanik Bhongbhibhat <email@thanik.me>
2023-04-01 08:40:34 +0700

    configure user.rpc dependency on order api

--- services/go-zero-demo/mall/order/api/internal/svc/servicecontext.go
+++ services/go-zero-demo/mall/order/api/internal/svc/servicecontext.go
@@ -2,14 +2,19 @@ package svc
 
 import (
 	"go-zero-demo/mall/order/api/internal/config"
+	"go-zero-demo/mall/user/rpc/types/user"
+
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config  config.Config
+	UserRpc user.UserClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:  c,
+		UserRpc: user.NewUserClient(zrpc.MustNewClient(c.UserRpc).Conn()),
 	}
 }
