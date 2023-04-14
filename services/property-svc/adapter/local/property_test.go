accd75cfcf816c41b99f9c1897e34e6509fb7a10
Thanik Bhongbhibhat <email@thanik.me>
2023-04-14 17:15:22 +0700

    TestPropertyRepository FindByID

---
+++ services/property-svc/adapter/local/property_test.go
@@ -0,0 +1,58 @@
+package local_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
+	"github.com/hausops/mono/services/property-svc/adapter/local"
+	"github.com/hausops/mono/services/property-svc/domain/property"
+)
+
+func TestPropertyRepository(t *testing.T) {
+	ctx := context.Background()
+
+	sample := local.ExampleProperties()
+
+	repo := local.NewPropertyRepository()
+	repo.ReplaceProperties(sample)
+
+	t.Run("FindByID", func(t *testing.T) {
+		testCases := []struct {
+			id   uuid.UUID
+			want property.Property
+			err  error
+		}{
+			{
+				id:   sample[0].GetID(),
+				want: sample[0],
+				err:  nil,
+			},
+			{
+				id:   sample[4].GetID(),
+				want: sample[4],
+				err:  nil,
+			},
+			{
+				id:   uuid.New(),
+				want: nil,
+				err:  property.ErrNotFound,
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.id.String(), func(t *testing.T) {
+				got, err := repo.FindByID(ctx, tc.id)
+
+				if err != tc.err {
+					t.Fatalf("want error %q but got %q", tc.err, err)
+				}
+
+				if diff := cmp.Diff(got, tc.want); diff != "" {
+					t.Fatalf("mismatch (-want +got):\n%s", diff)
+				}
+			})
+		}
+	})
+}
