2cbead84ac6d4904481a2d3ba4ceaf2d073bd748
Thanik Bhongbhibhat <email@thanik.me>
2023-06-19 11:31:34 +0700

    config supports using different datastores

--- services/auth-svc/config/config.go
+++ services/auth-svc/config/config.go
@@ -10,7 +10,43 @@ import (
 )
 
 type Config struct {
-	Mode mode
+	Mode      mode
+	Datastore datastore
+}
+
+func (c *Config) UnmarshalYAML(node *yaml.Node) error {
+	var conf struct {
+		Mode      mode                 `yaml:"mode"`
+		Datastore map[string]yaml.Node `yaml:"datastore"`
+	}
+
+	if err := node.Decode(&conf); err != nil {
+		return fmt.Errorf("decode config from YAML: %w", err)
+	}
+
+	// Set mode
+	c.Mode = conf.Mode
+
+	// Set datastore
+	switch kind := conf.Datastore["kind"].Value; kind {
+	case "local":
+		c.Datastore = LocalDatastore{}
+	case "redis":
+		var redis RedisDatastore
+		spec := conf.Datastore["spec"]
+		if err := spec.Decode(&redis); err != nil {
+			return fmt.Errorf("decode redis datastore spec from YAML: %w", err)
+		}
+		c.Datastore = redis
+	default:
+		return fmt.Errorf("unknown datastore kind: %s", kind)
+	}
+
+	if err := c.Validate(); err != nil {
+		return fmt.Errorf("validate config: %w", err)
+	}
+
+	return nil
 }
 
 func (c *Config) Validate() error {
@@ -18,6 +54,16 @@ func (c *Config) Validate() error {
 	case ModeProd, ModeDev:
 	default:
 		return fmt.Errorf("unknown mode: %v", c.Mode)
+	}
+
+	switch t := c.Datastore.(type) {
+	case LocalDatastore:
+	case RedisDatastore:
+		if t.URI == "" {
+			return errors.New("redis datastore missing URI field")
+		}
+	default:
+		return fmt.Errorf("unknown datastore: %v", c.Datastore)
 	}
 
 	return nil
@@ -55,3 +101,18 @@ const (
 	ModeProd mode = "prod"
 	ModeDev  mode = "dev"
 )
+
+type datastore interface {
+	isDatastore()
+}
+
+type LocalDatastore struct{}
+
+func (d LocalDatastore) isDatastore() {}
+
+type RedisDatastore struct {
+	// URI is the redis connection URI.
+	URI string `yaml:"uri"`
+}
+
+func (d RedisDatastore) isDatastore() {}
