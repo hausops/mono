2ba3a7dbb30c46661977fbcb4dbc0a9cbe2c3c2a
Thanik Bhongbhibhat <email@thanik.me>
2023-06-19 09:21:53 +0700

    session repo: handle redis.HGetAll does not return redis.Nil when key not found - wraps FindByAccessToken, FindByEmail in watch transaction - change access-token index to simple key-value instead of hash so we know when key doesn't exist

--- services/auth-svc/adapter/redis/session.go
+++ services/auth-svc/adapter/redis/session.go
@@ -28,14 +28,15 @@ func (r *sessionRepository) DeleteByEmail(ctx context.Context, email mail.Addres
 	}
 
 	primaryKey := r.primaryKey(email)
+	accessTokenKey := r.accessTokenKey(sess.AccessToken)
 	err = r.client.Watch(ctx, func(tx *redis.Tx) error {
 		pipe := tx.TxPipeline()
 		pipe.Del(ctx, primaryKey)
-		pipe.HDel(ctx, r.accessTokensKey(), sess.AccessToken.String())
+		pipe.Del(ctx, accessTokenKey)
 
 		_, err = pipe.Exec(ctx)
 		return err
-	}, primaryKey)
+	}, primaryKey, accessTokenKey)
 
 	if err != nil {
 		return session.Session{}, err
@@ -44,58 +45,77 @@ func (r *sessionRepository) DeleteByEmail(ctx context.Context, email mail.Addres
 }
 
 func (r *sessionRepository) FindByAccessToken(ctx context.Context, token session.AccessToken) (session.Session, error) {
-	emailAddr, err := r.client.HGet(ctx, r.accessTokensKey(), token.String()).Result()
-	switch {
-	case errors.Is(err, redis.Nil):
-		return session.Session{}, session.ErrNotFound
-	case err != nil:
-		return session.Session{},
-			fmt.Errorf("get email from access token %s: %w", token, err)
-	}
+	accessTokenKey := r.accessTokenKey(token)
 
-	return r.FindByEmail(ctx, mail.Address{Address: emailAddr})
+	var sess session.Session
+	err := r.client.Watch(ctx, func(tx *redis.Tx) error {
+		emailAddr, err := tx.Get(ctx, r.accessTokenKey(token)).Result()
+		switch {
+		case errors.Is(err, redis.Nil):
+			return session.ErrNotFound
+		case err != nil:
+			return fmt.Errorf("get email from access token %s: %w", token, err)
+		}
+
+		email, err := mail.ParseAddress(emailAddr)
+		if err != nil {
+			return fmt.Errorf("parse email address: %w", err)
+		}
+
+		sess, err = r.FindByEmail(ctx, *email)
+		if err != nil {
+			return fmt.Errorf("FindByEmail(%s): %w", email.Address, err)
+		}
+		return nil
+	}, accessTokenKey)
+
+	return sess, err
 }
 
 func (r *sessionRepository) FindByEmail(ctx context.Context, email mail.Address) (session.Session, error) {
 	primaryKey := r.primaryKey(email)
 
-	var saved sessionRedis
-	err := r.client.HGetAll(ctx, primaryKey).Scan(&saved)
-	switch {
-	case errors.Is(err, redis.Nil):
-		return session.Session{}, session.ErrNotFound
-	case err != nil:
-		return session.Session{}, fmt.Errorf("redis.HGetAll(%s): %w", primaryKey, err)
-	}
+	var sess session.Session
+	err := r.client.Watch(ctx, func(tx *redis.Tx) error {
+		// We need to check with Exists before calling HGetAll because,
+		// when the key doesn't exist HGetAll returns nil error, instead of redis.Nil.
+		// https://github.com/redis/go-redis/issues/1668
+		n, err := tx.Exists(ctx, primaryKey).Result()
+		if err != nil {
+			return fmt.Errorf("redis.Exists(%s): %w", primaryKey, err)
+		} else if n == 0 {
+			return session.ErrNotFound
+		}
 
-	// Ensure that we check this condition after checking err != nil from HGetAll.
-	// Otherwise, we'll always hit it when there's an error.
-	if saved.AccessToken == "" {
-		return session.Session{}, session.ErrNotFound
-	}
+		var saved sessionRedis
+		err = tx.HGetAll(ctx, primaryKey).Scan(&saved)
+		if err != nil {
+			return fmt.Errorf("redis.HGetAll(%s): %w", primaryKey, err)
+		}
 
-	token, err := session.ParseAccessToken(saved.AccessToken)
-	if err != nil {
-		return session.Session{}, err
-	}
+		token, err := session.ParseAccessToken(saved.AccessToken)
+		if err != nil {
+			return err
+		}
 
-	sess := session.Session{
-		AccessToken: token,
-		Email:       email,
-		ExpireAt:    time.Unix(0, saved.ExpireAt),
-	}
-	return sess, nil
+		sess = session.Session{
+			AccessToken: token,
+			Email:       email,
+			ExpireAt:    time.Unix(0, saved.ExpireAt),
+		}
+		return nil
+	}, primaryKey)
+
+	return sess, err
 }
 
 func (r *sessionRepository) Upsert(ctx context.Context, sess session.Session) error {
 	primaryKey := r.primaryKey(sess.Email)
 	// Watch the primary key to detect changes by other clients
 	return r.client.Watch(ctx, func(tx *redis.Tx) error {
-		prevToken, err := tx.HGet(ctx, primaryKey, "accessToken").Result()
-		switch {
-		case errors.Is(err, redis.Nil):
-			// ok
-		case err != nil:
+		// Get the current access token or empty string
+		prevTokenStr, err := tx.HGet(ctx, primaryKey, "accessToken").Result()
+		if err != nil && !errors.Is(err, redis.Nil) {
 			return fmt.Errorf("redis.HGet(%s, accessToken): %w", primaryKey, err)
 		}
 
@@ -105,8 +125,16 @@ func (r *sessionRepository) Upsert(ctx context.Context, sess session.Session) er
 			ExpireAt:    sess.ExpireAt.UnixNano(),
 		})
 
-		pipe.HDel(ctx, r.accessTokensKey(), prevToken)
-		pipe.HSet(ctx, r.accessTokensKey(), sess.AccessToken.String(), sess.Email.Address)
+		// If updating, remove the previous access token index for the session.
+		if prevTokenStr != "" {
+			prevToken, err := session.ParseAccessToken(prevTokenStr)
+			if err != nil {
+				return fmt.Errorf("parse previously stored access token: %w", err)
+			}
+			pipe.Del(ctx, r.accessTokenKey(prevToken))
+		}
+
+		pipe.Set(ctx, r.accessTokenKey(sess.AccessToken), sess.Email.Address, 0)
 
 		_, err = pipe.Exec(ctx)
 		return err
@@ -117,8 +145,8 @@ func (r *sessionRepository) primaryKey(email mail.Address) string {
 	return fmt.Sprintf("auth-svc:session-repo:email:%s", email.Address)
 }
 
-func (r *sessionRepository) accessTokensKey() string {
-	return "auth-svc:session-repo:access-tokens"
+func (r *sessionRepository) accessTokenKey(token session.AccessToken) string {
+	return fmt.Sprintf("auth-svc:session-repo:access-token-idx:%s", token)
 }
 
 // sessionRedis represents stored session data for a given key in redis.
