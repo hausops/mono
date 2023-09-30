3ec7cae5a8b8970d1082222c00a477faa11a40bd
Thanik Bhongbhibhat <mail@thanik.me>
2023-09-30 09:03:44 +0700

    Run prettier -w

--- apps/dashboard/volto/Radio/Radio.tsx
+++ apps/dashboard/volto/Radio/Radio.tsx
@@ -5,16 +5,15 @@ import * as s from './Radio.css';
 type RadioProps = ComponentPropsWithRef<'input'>;
 
 // Radio is a custom styled radio input to render consistently in all browsers.
-export const Radio = forwardRef<HTMLInputElement, RadioProps>(function Radio(
-  props,
-  ref
-) {
-  return (
-    <input
-      {...props}
-      type="radio"
-      className={clsx(s.Radio, props.className)}
-      ref={ref}
-    />
-  );
-});
+export const Radio = forwardRef<HTMLInputElement, RadioProps>(
+  function Radio(props, ref) {
+    return (
+      <input
+        {...props}
+        type="radio"
+        className={clsx(s.Radio, props.className)}
+        ref={ref}
+      />
+    );
+  }
+);
